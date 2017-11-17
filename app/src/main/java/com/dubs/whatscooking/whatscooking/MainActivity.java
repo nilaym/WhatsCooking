a506459b2a6c20d4e9d9b1c0e2a008a89868ec73
lawtondy <lawtondy@umich.edu>
2017-11-17 11:24:55 -0500

    onclick set up for buttons

--- app/src/main/java/com/dubs/whatscooking/whatscooking/MainActivity.java
+++ app/src/main/java/com/dubs/whatscooking/whatscooking/MainActivity.java
@@ -21,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final Button buttonPandaExpress = findViewById(R.id.b_panda);
+        buttonPandaExpress.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
+                intent.putExtra("NAME", "PandaExpress");
+                startActivity(intent);
+            }
+        });
+
+        final Button buttonPizzaHouse = findViewById(R.id.b_pizza);
+        buttonPizzaHouse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
+                intent.putExtra("NAME", "PizzaHouse");
+                startActivity(intent);
+            }
+        });
+
+        final Button buttonChipotle = findViewById(R.id.b_chipotle);
+        buttonChipotle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
+                intent.putExtra("NAME", "Chipotle");
+                startActivity(intent);
+            }
+        });
+
+        final Button buttonPiada = findViewById(R.id.b_piada);
+        buttonChipotle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
+                intent.putExtra("NAME", "Piada");
+                startActivity(intent);
+            }
+        });
     }
 }
