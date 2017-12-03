53d5de1bc529eb8d7d06b00341d1d2d325e0ed86
Nilay <nmuchhala415@gmail.com>
2017-12-03 15:47:52 -0500

    updated UI for restaurant selector

--- app/src/main/java/com/dubs/whatscooking/whatscooking/MainActivity.java
+++ app/src/main/java/com/dubs/whatscooking/whatscooking/MainActivity.java
@@ -2,60 +2,56 @@ package com.dubs.whatscooking.whatscooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
+import com.beardedhen.androidbootstrap.TypefaceProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TypefaceProvider.registerDefaultIconSets();
 
-        final Button buttonNoThai = findViewById(R.id.b_nothai);
-        buttonNoThai.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
-                intent.putExtra("NAME", "NoThai");
-                startActivity(intent);
-            }
-        });
+        List<Integer> thumbIds = new ArrayList<>(Arrays.asList(R.id.chiptole_thumb,
+                                                        R.id.no_thai_thumb, R.id.panda_express_thumb,
+                                                        R.id.piada_thumb, R.id.pizza_house_thumb));
 
-        final Button buttonPandaExpress = findViewById(R.id.b_panda);
-        buttonPandaExpress.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
-                intent.putExtra("NAME", "PandaExpress");
-                startActivity(intent);
-            }
-        });
+        final List<String> extras = new ArrayList<>(Arrays.asList("Chipotle", "NoThai",
+                                                            "PandaExpress", "Piada",
+                                                            "PizzaHouse"));
 
-        final Button buttonPizzaHouse = findViewById(R.id.b_pizza);
-        buttonPizzaHouse.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
-                intent.putExtra("NAME", "PizzaHouse");
-                startActivity(intent);
-            }
-        });
+        for (int i = 0; i < thumbIds.size(); i++) {
+            final BootstrapCircleThumbnail thumbnail = findViewById(thumbIds.get(i));
+            final int index = i;  // copying into final b/c of access in onClick()
+            thumbnail.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
+                    intent.putExtra("NAME", extras.get(index));
+                    startActivity(intent);
+                }
+            });
+            thumbnail.setOnTouchListener(this);
+        }
+    }
 
-        final Button buttonChipotle = findViewById(R.id.b_chipotle);
-        buttonChipotle.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
-                intent.putExtra("NAME", "Chipotle");
-                startActivity(intent);
-            }
-        });
+    @Override
+    public boolean onTouch(View v, MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+            v.setAlpha(.5f);
+        } else {
+            v.setAlpha(1f);
+        }
 
-        final Button buttonPiada = findViewById(R.id.b_piada);
-        buttonPiada.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RestaurantMenuActivity.class);
-                intent.putExtra("NAME", "Piada");
-                startActivity(intent);
-            }
-        });
+        return false;
     }
 }
