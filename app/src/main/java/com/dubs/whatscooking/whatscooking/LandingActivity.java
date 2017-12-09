779e2763d93ee542d07c18743ec32a0f9ae51d9d
Nilay <nmuchhala415@gmail.com>
2017-12-09 15:18:42 -0500

    tried getting hyperlink to yummly working

--- app/src/main/java/com/dubs/whatscooking/whatscooking/LandingActivity.java
+++ app/src/main/java/com/dubs/whatscooking/whatscooking/LandingActivity.java
@@ -1,11 +1,18 @@
 package com.dubs.whatscooking.whatscooking;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class LandingActivity extends AppCompatActivity {
 
